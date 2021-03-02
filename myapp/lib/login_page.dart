7522ae707310a5179e171500f7f29c5f1ba94b97
Guilherme Lourenço <gui.lourenzo@gmail.com>
2021-03-02 13:42:12 -0300

    teste de login

---
+++ myapp/lib/login_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WiseFather Login"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              Text(
+                'Informações de Login',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                  onSaved: (value) => _email = value,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(labelText: "Digite seu E-mail")),
+              SizedBox(height: 20.0),
+              TextFormField(
+                  onSaved: (value) => _password = value,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: "Digite sua Senha")),
+              RaisedButton(
+                  child: Text("LOGIN"),
+                  onPressed: () {
+                    final form = _formKey.currentState;
+                    form.save();
+
+                    // Validate will return true if is valid, or false if invalid.
+                    if (form.validate()) {
+                      print("$_email $_password");
+                    }
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
